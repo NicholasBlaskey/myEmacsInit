399f907a2844986c9946fa0548d99c48d90e7740
Nick Blaskey <nicholas.blaskey1@marist.edu>
2019-10-17 15:51:38 -0400

    Added my emacs init file

---
+++ init.el
@@ -0,0 +1,22 @@
+(column-number-mode 1)
+(setq-default indent-tabs-mode nil)
+(load-theme 'naysayer)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   '(dockerfile-mode yaml-mode ace-flyspell scala-mode cobol-mode cobalt php-mode ein go-mode rust-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
