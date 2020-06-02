761e41a364d5fea92b6474a60a8b4c43338196c7
Nick Blaskey <nicholas.blaskey1@marist.edu>
2020-06-02 15:27:46 -0400

    Updated emacs init

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (column-number-mode 1)
 (setq-default indent-tabs-mode nil)
 (load-theme 'naysayer)
+(setq-default tab-width 4)
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
@@ -12,11 +13,14 @@
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(dockerfile-mode yaml-mode ace-flyspell scala-mode cobol-mode cobalt php-mode ein go-mode rust-mode)))
+   '(tabbar ## glsl-mode gle-mode erlang dockerfile-mode yaml-mode ace-flyspell scala-mode cobol-mode cobalt php-mode ein go-mode rust-mode)))
+(global-set-key (kbd "<C-tab>") 'tabbar-forward)
+(global-set-key (kbd "<C-S-tab>") 'tabbar-backward)
+(add-hook 'before-save-hook #'gofmt-before-save)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
+(tabbar-mode)
